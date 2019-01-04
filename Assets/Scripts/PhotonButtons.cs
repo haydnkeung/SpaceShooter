06a6ce6574cec73055f804ba7985a635a746c4ed
haydnkeung <haydnkeung@gmail.com>
2019-01-03 17:57:09 -0800

    Photon Project Init

---
+++ Assets/Scripts/PhotonButtons.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonButtons : MonoBehaviour
+{
+
+    public InputField joinServerInput, createServerInput;
+    public MenuScript menuScript;
+
+    public void createRoom() {
+        RoomOptions options = new RoomOptions { MaxPlayers = 3 };
+        
+            PhotonNetwork.CreateRoom(joinServerInput.text, options, null);//Null?
+       
+        
+    }
+
+    public void joinRoom() {
+        PhotonNetwork.JoinRoom(joinServerInput.text);
+    }
+
+
+    private void OnJoinedRoom() {
+        menuScript.disableMenuUI();
+        Debug.Log("Joined room " + joinServerInput.text);
+    }
+
+
+    private void OnCreateRoom() {
+       
+        Debug.Log("Created room " + createServerInput.text);
+        //Debug.Log("Joined room " + joinServerInput.text);
+    }
+
+}
---
+++ Assets/Scripts/PhotonConnect.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonConnect : MonoBehaviour
+{
+    public string versionName = "1.0";
+
+    public GameObject Sectionview1, Sectionview2, Sectionview3;
+
+
+    public void connectToPhoton() {
+        PhotonNetwork.ConnectUsingSettings(versionName);
+        Debug.Log("Connecting to Photon");
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected to master");
+    }
+
+    private void OnJoinedLobby() {
+        Sectionview1.SetActive(false);
+        Sectionview2.SetActive(true);
+        Debug.Log("Joined Lobby");
+    }
+
+
+    private void OnDisconnectedFromPhoton()
+    {
+        Sectionview1.SetActive(false);
+        Sectionview2.SetActive(false);
+        Sectionview3.SetActive(true);
+        
+    }
+
+
+    private void OnFailedToConnectToPhoton()
+    {
+        
+    }
+}
